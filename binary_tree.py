0b39c83f3b487affec98fae25f14e63e83ed0c79
jaewie <jaehyun.wie@mail.utoronto.ca>
2013-05-29 19:01:25 -0400

    added find_node

--- binary_tree.py
+++ binary_tree.py
@@ -1,6 +1,6 @@
 class Node(object):
     def __init__(self, d):
-        self.d = d
+        self.data = d
         self.left = None
         self.right = None
         self.parent = None
@@ -21,7 +21,7 @@ def bfs(root):
     res = []
     while queue:
         node = queue.pop(0)
-        res.append(node.d)
+        res.append(node.data)
         if node.left:
             queue.append(node.left)
         if node.right:
@@ -35,34 +35,31 @@ def dfs(root):
     res = []
     while stack:
         node = stack.pop()
-        res.append(node.d)
+        res.append(node.data)
         if node.right:
             stack.append(node.right)
         if node.left:
             stack.append(node.left)            
     return res
     
-
-
 def is_BST(root):
-    '''Check if root is a binary search tree.'''    
+    '''Return whether root is a binary search tree.'''
+
     lst = in_order(root)
     
-    for i in range(len(lst) -1):
+    for i in range(len(lst) - 1):
         if lst[i] > lst[i+1]:
             return False
     return True
 
 def rec_DFS(node, lst=[]):
     '''Return a DFS of node recursively'''
-
-    if node in lst:
-        return True
-    lst.append(node.d)
-    if node.left:
-        rec_DFS(node.left)
-    if node.right:
-        rec_DFS(node.right)
+    if node:
+        lst.append(node.data)
+        if node.left:
+            rec_DFS(node.left)
+        if node.right:
+            rec_DFS(node.right)
     return lst
 
 def in_order_predc(node):
@@ -82,44 +79,41 @@ def in_order_predc(node):
 
 def insert_into_BST(root, node):
     ''' Insert node into BST root. It's assumed all values are distinct'''
-    if not root:
-        root = node
     
     if is_leaf(root):
-        if root.d > node.d:
-            root.left = node
-        else:
+        if root.data < node.data:
             root.right = node
-    else: # Otherwise, traverse through the BST
-        if root.d > node.d:
-            insert_into_BST(root.left, node)
         else:
+            root.left = node
+        node.parent = root        
+    else:
+        if root.data < node.data:
             insert_into_BST(root.right, node)
+        else:
+            insert_into_BST(root.left, node)
+    
+def find_node(node, val):
+    if node:
+        if node.data < val:
+            return find_node(node.right, val)
+        elif node.data > val:
+            return find_node(node.left, val)
+        else:
+            return node
 
 def is_leaf(node):
     ''' Return whether node is a leaf.'''
 
     return node and not node.left and not node.right
     
-def print_perimeter(node):
-    '''Print the perimeter of tree rooted at node.'''
-    
-    copy = node    
-    while node.left and not is_leaf(copy):
-        print copy.v
-        copy = copy.left
-    print_leaves(node)
-    copy = node
-    while node.right and not is_leaf(copy):
-        print co.v
-        copy = copy.right
+def height(node):
+    '''Return the height of the tree rooted at node.'''
 
-def print_leaves(node):
-    '''Print the leaves of node.'''
     if node:
-        if is_leaf(node):
-            print node.v
-        else:
-            print_leaves(node.left)
-            print_leaves(node.right)
+        return 1 + max(height(node.left), height(node.right))
+    return 0
 
+if __name__ == "__main__":
+    a = Node(5)
+
+                
