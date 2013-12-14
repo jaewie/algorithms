a936774249e421815f1061c1482bb2248459216c
jae <jae@z.(none)>
2013-12-14 16:34:13 -0500

    fixed quick_select to take in non-unique numbers

--- arrays.py
+++ arrays.py
@@ -14,17 +14,22 @@ def quick_select(lst, n):
     '''Return the nth smallest number in lst using quick select.'''
     
     if not lst or len(lst) < n:
-        return None
+        raise IndexError("There is no nth number in lst")
     
     piv = lst[random.randint(0, len(lst) - 1)]
     less = [x for x in lst if x < piv]
+    same = [x for x in lst if x == piv]
     bigger = [x for x in lst if x > piv]
     
+    # [1, 2, 3] [4, 4], [5, 6 , 7]
     
-    if len(less) == n - 1:
-        return piv
+    # if n is between 3 - 4 return pivot
+    # if n is less than 3 return qs of left, n
+    # otherwise return qs of bigger, n - len(less) and len(same)
     
-    if len(less) >= n:
+    if n < len(less):
         return quick_select(less, n)
+    elif n < len(less) + len(same):
+        return piv
     else:
-        return quick_select(bigger, n - 1 - len(less))
+        return quick_select(bigger, n - len(same) - len(less))
