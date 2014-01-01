14699d93bae7d3fcf0fe8b90b4c2e0d9aa800e5c
jae <jae@z.(none)>
2013-12-31 19:28:35 -0500

    refactor queue

--- collection/queue.py
+++ collection/queue.py
@@ -1,37 +1,36 @@
 class Queue(object):
     def __init__(self):
-        self.head = None
-        self.tail = None
-        
+        self._head = None
+        self._tail = None
+    
+    class Node(object):
+        def __init__(self, val):
+            self.val = val
+            self.next = None
+            self.prev = None    
+    
     def enqueue(self, val):
-        node = Node(val)
+        node = self.Node(val)
         
-        if not self.tail:
-            self.head = self.tail = node
+        if not self._tail:
+            self._head = self._tail = node
         else:
-            self.tail.next = node
-            node.prev = self.tail
-            self.tail = self.tail.next
+            self._tail.next = node
+            node.prev = self._tail
+            self._tail = self._tail.next
             
     def dequeue(self):
-        if not self.head:
+        if not self._head:
             raise Exception("Dequeue from empty queue")
         
         node = self.peek()
         
-        if self.head == self.tail:
-            self.head = self.tail = None
+        if self._head == self._tail:
+            self._head = self._tail = None
         else:
-            self.head = self.head.next
-            self.head.prev = None
+            self._head = self._head.next
+            self._head.prev = None
         return node.val
 
     def peek(self):
-        return self.head
-
-
-class Node(object):
-    def __init__(self, val):
-        self.val = val
-        self.next = None
-        self.prev = None
+        return self._head
