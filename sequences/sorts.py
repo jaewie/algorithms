f36abdea6516fc7e5ef36ef3f1dae71ed51b88a5
Jae Wie <jaehyun.wie@mail.utoronto.ca>
2014-01-04 18:17:17 -0500

    refactor is_sorted func

--- sequences/sorts.py
+++ sequences/sorts.py
@@ -177,4 +177,4 @@ def to_list(queue):
     return lst
 
 def is_sorted(lst):
-    return len(lst) <= 1 or all(lst[i] <= lst[i + 1] for i in range(len(lst) - 1))
+    return all(lst[i] <= lst[i + 1] for i in range(len(lst) - 1))
