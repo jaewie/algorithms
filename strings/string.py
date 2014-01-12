445f1e6b623b299f1c65039dcd2a37b05ed8c7a6
Jae Wie <jaehyun.wie@mail.utoronto.ca>
2014-01-12 00:13:59 -0500

    prime constants shouldn't have been globals

--- strings/string.py
+++ strings/string.py
@@ -1,6 +1,3 @@
-PRIME_MOD = 257
-PRIME_BASE = 1000000007
-
 def substr(s, sub):
     sub_hash = roll_hash(sub)
     check_hash = None
@@ -9,21 +6,20 @@ def substr(s, sub):
         prev_char = s[i - 1] if i > 0 else None
         check_hash = roll_hash(check, prev_char, check_hash)
         
-        if (roll_hash(check), check) == (sub_hash, sub):
+        if roll_hash(check), check == sub_hash, sub:
             return i
     return -1
 
-def roll_hash(s, prev_char=None, prev_hash=None, prime_mod=PRIME_MOD,
-              prime_base=PRIME_BASE):
+def roll_hash(s, prev_char=None, prev_hash=None, prime_mod=257,
+              prime_base=1000000007):
     k = len(s) - 1
     if prev_hash is not None:
-        return shift_hash_right(k, prev_hash, prev_char)
+        return shift_hash_right(k, prev_hash, prev_char, prime_mod, prime_base)
 
     val = sum(ord(char) * prime_mod ** (k - i) for i, char in enumerate(s))
     return val % prime_base
 
-def shift_hash_right(k, prev_hash, prev_char, prime_mod=PRIME_MOD,
-              prime_base=PRIME_BASE):
+def shift_hash_right(k, prev_hash, prev_char, prime_mod, prime_base):
     prev_hash -= ord(prev_char) * prime_mod ** (k)
     prev_hash *= prime_mod
     prev_hash += ord(k)
