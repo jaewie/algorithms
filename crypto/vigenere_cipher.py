0756ccb09f514828246e512471cf9b6cb8b1e7ab
Jae Wie <jaehyun.wie@mail.utoronto.ca>
2014-03-07 18:57:00 -0500

    added testing, and crypto dir

---
+++ crypto/vigenere_cipher.py
@@ -0,0 +1,13 @@
+from string import uppercase
+from itertools import cycle, starmap, izip
+
+
+def vigenere_cipher(msg, key):
+  letter_to_val = {char: val for val, char in enumerate(uppercase)}
+  val_to_letter = {val: char for val, char in enumerate(uppercase)}
+
+  get_val = lambda c0, c1: (letter_to_val[c0] + letter_to_val[c1]) % len(uppercase)
+  get_cipher_char = lambda c0, c1: val_to_letter[get_val(c0, c1)]
+  key = cycle(key) if len(msg) > len(key) else key
+
+  return ''.join(c for c in starmap(get_cipher_char, izip(msg, key)))
---
+++ run_tests.py
@@ -0,0 +1,24 @@
+from tests import test_deque
+import os
+import sys
+import unittest
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+def get_test_suite(test_modules):
+  suite = unittest.TestSuite()
+  for t in test_modules:
+      try:
+          # If the module defines a suite() function, call it to get the suite.
+          mod = __import__(t, globals(), locals(), ['suite'])
+          suitefn = getattr(mod, 'suite')
+          suite.addTest(suitefn())
+      except (ImportError, AttributeError):
+          # else, just load all the test cases from the module.
+          suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
+  return suite
+
+if __name__ == "__main__":
+  test_modules = ['tests.test_deque']
+  suite = get_test_suite(test_modules)
+  unittest.TextTestRunner().run(suite)
