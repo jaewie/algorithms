619427cd3f64e20ee00d54baaeafa31a96a2a70b
Jae Wie <jaehyun.wie@mail.utoronto.ca>
2014-03-07 21:13:18 -0500

    oops fixing out of range bug

--- crypto/one_time_pad.py
+++ crypto/one_time_pad.py
@@ -6,6 +6,6 @@ from random import randint
 
 def one_time_pad(msg, pad=None):
   if pad is None:
-    pad = (randint(0, len(lowercase)) for _ in range(len(msg)))
+    pad = (randint(0, len(lowercase) - 1) for _ in range(len(msg)))
 
   return ''.join(shift_char(char, shift) for char, shift in izip(msg, pad))
