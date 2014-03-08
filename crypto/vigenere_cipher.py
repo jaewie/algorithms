3e30c1d013688eb6b01acf5838c170a5b952d5ad
Jae Wie <jaehyun.wie@mail.utoronto.ca>
2014-03-07 20:40:09 -0500

    change uppercase to lowercase in vigenere cipher

--- crypto/vigenere_cipher.py
+++ crypto/vigenere_cipher.py
@@ -1,13 +1,13 @@
-from string import uppercase
+from string import lowercase
 from itertools import cycle, starmap, izip
 
 
 def vigenere_cipher(msg, key):
-  msg = msg.upper()
-  letter_to_val = {char: val for val, char in enumerate(uppercase)}
-  val_to_letter = {val: char for val, char in enumerate(uppercase)}
+  msg = msg.lower()
+  letter_to_val = {char: val for val, char in enumerate(lowercase)}
+  val_to_letter = {val: char for val, char in enumerate(lowercase)}
 
-  get_val = lambda c0, c1: (letter_to_val[c0] + letter_to_val[c1]) % len(uppercase)
+  get_val = lambda c0, c1: (letter_to_val[c0] + letter_to_val[c1]) % len(lowercase)
   get_cipher_char = lambda c0, c1: val_to_letter[get_val(c0, c1)]
   key = cycle(key) if len(msg) > len(key) else key
 
