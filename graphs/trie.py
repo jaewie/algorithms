c30a47ab2d2d5361c3e842458e90a1afcc8bcb86
Jae Wie <jaehyun.wie@mail.utoronto.ca>
2014-03-09 11:26:49 -0400

    fixing bug with trie

--- graphs/trie.py
+++ graphs/trie.py
@@ -18,14 +18,18 @@ class Trie(object):
     return True
 
   def __str__(self):
-    return ', '.join(__get_strs(self._trie))
+    return ', '.join(self.__get_strs(self._trie))
 
-  def __get_strs(d):
+  def __get_strs(self, d):
     if not d:
       return ['']
 
     res = []
     for char in d:
-      child_res = get_strs(d[char])
+      child_res = self.__get_strs(d[char])
       res.extend([char + s for s in child_res])
     return res
+
+a = Trie()
+a.insert("hello")
+print a
