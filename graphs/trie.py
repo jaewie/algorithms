c2ad852a14f9b387bbde2cd2db795ad5d83962ea
Jae Wie <jaehyun.wie@mail.utoronto.ca>
2014-03-09 11:23:02 -0400

    adding a trie

---
+++ graphs/trie.py
@@ -0,0 +1,31 @@
+class Trie(object):
+  def __init__(self):
+    self._trie = {}
+
+  def insert(self, text):
+    trie = self._trie
+    for char in text:
+      if char not in trie:
+        trie[char] = {}
+      trie = trie[char]
+  
+  def exists(self, text):
+    trie = self._trie
+    for char in text:
+      if char not in trie:
+        return False
+      trie = trie[char]
+    return True
+
+  def __str__(self):
+    return ', '.join(__get_strs(self._trie))
+
+  def __get_strs(d):
+    if not d:
+      return ['']
+
+    res = []
+    for char in d:
+      child_res = get_strs(d[char])
+      res.extend([char + s for s in child_res])
+    return res
