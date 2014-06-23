cb9419d53c358f9df07d108fccf76e7587671f7c
Jae Wie <jaehyun.wie@mail.utoronto.ca>
2014-06-22 22:46:39 -0400

    adding faster count_bits algo.

--- bitwise/bit_manipulations.py
+++ bitwise/bit_manipulations.py
@@ -1,5 +1,10 @@
 def count_bits(n):
+    '''Takes 32 iterations for 32 bit int.'''
     return (n & 1) + count_bits(n >> 1) if n else 0
+
+def count_bits(n):
+    '''Takes k iterations where k is the number of bits set.'''
+    return 1 + count_bits(n & (n - 1)) if n else 0
 
 def is_power_of_two(n):
     return not (n & (n - 1))
