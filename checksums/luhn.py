a56a48c74964e718e5be352168b2009b82c6872f
Jae Wie <jaehyun.wie@mail.utoronto.ca>
2014-07-06 19:57:16 -0400

    adding luhn checksum

---
+++ checksums/luhn.py
@@ -0,0 +1,10 @@
+def luhn(num):
+    '''The Luhn algorithm checksum formula used to validate id numbers'''
+    num = list(map(int, str(num)))
+    sum_digits = lambda num: sum(map(int, str(num)))
+
+    odd_digits = list(map(lambda x: sum_digits(x * 2), num[-1::-2]))
+    even_digits = num[-2::-2] 
+
+    check_digit = (sum(odd_digits) + sum(even_digits)) % 10
+    return check_digit
