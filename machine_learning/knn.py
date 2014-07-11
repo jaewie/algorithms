be7f5cfdc7799727089b8fcbed174bbe089d4184
Jae Wie <jaehyun.wie@mail.utoronto.ca>
2014-07-10 23:54:39 -0400

    refactor to use itemgetter

--- machine_learning/knn.py
+++ machine_learning/knn.py
@@ -7,8 +7,8 @@ from collections import Counter
 def k_nearest_neighbor(dataset, querypt, k):
     euclidean = lambda x, y: return sum((i - j) ** 2 for i, j in zip(x, y))
     dist_querypt = partial(euclidean, y=querypt)
-    get_feature = lambda e: e[0]
-    get_classification = lambda e: e[1]
+    get_feature = itemgetter(0)
+    get_classification = itemgetter(1)
 
     all_nearest = sorted(dataset, key=lambda e: dist_querypt(get_feature(e)))
     k_nearest = all_nearest[:k]
