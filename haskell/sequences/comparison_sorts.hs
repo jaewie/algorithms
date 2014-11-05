7c2d09966f84f62458a9fe82ef02122e4f3541bd
Jae Wie <jaehyun.wie@mail.utoronto.ca>
2014-11-05 14:46:19 -0500

    first haskell algos

---
+++ haskell/sequences/comparison_sorts.hs
@@ -0,0 +1,26 @@
+import Data.List
+
+qsort :: (Ord a) => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = let le = [e | e <- xs, e < x]
+                   gt = [e | e <- xs, e > x]
+                   sm = [e | e <- xs, e == x] ++ [x]
+               in (qsort le) ++ sm ++ (qsort gt)
+
+msort :: (Ord a) => [a] -> [a]
+msort xs 
+    | len <= 1 = xs
+    | otherwise = merge (msort left) (msort right)
+    where
+        len = length xs
+        mid = div len 2
+        left = take mid xs
+        right = drop mid xs
+
+-- Helpers
+merge :: (Ord a) => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys)
+    | x < y = x:(merge xs (y:ys))
+    | otherwise = y:(merge (x:xs) ys)
