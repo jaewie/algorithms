f8412ae43c21bc589e2db218022d809f21e1444f
Jae Wie <jaehyun.wie@mail.utoronto.ca>
2014-12-28 21:52:57 -0500

    fix spacing

--- python/crypto/ciphers.py
+++ python/crypto/ciphers.py
@@ -5,21 +5,21 @@ from itertools import cycle, starmap, izip
 
 
 def vigenere_cipher(msg, key):
-  msg = msg.lower()
-  letter_to_val = {char: val for val, char in enumerate(lowercase)}
-  val_to_letter = {val: char for val, char in enumerate(lowercase)}
+    msg = msg.lower()
+    letter_to_val = {char: val for val, char in enumerate(lowercase)}
+    val_to_letter = {val: char for val, char in enumerate(lowercase)}
 
-  get_val = lambda c0, c1: (letter_to_val[c0] + letter_to_val[c1]) % len(lowercase)
-  get_cipher_char = lambda c0, c1: val_to_letter[get_val(c0, c1)]
-  key = cycle(key) if len(msg) > len(key) else key
+    get_val = lambda c0, c1: (letter_to_val[c0] + letter_to_val[c1]) % len(lowercase)
+    get_cipher_char = lambda c0, c1: val_to_letter[get_val(c0, c1)]
+    key = cycle(key)
 
-  return ''.join(c for c in starmap(get_cipher_char, izip(msg, key)))
+    return ''.join(c for c in starmap(get_cipher_char, izip(msg, key)))
 
 def one_time_pad(msg, pad=None):
-  if pad is None:
-    pad = (randint(0, len(lowercase) - 1) for _ in range(len(msg)))
+    if pad is None:
+        pad = (randint(0, len(lowercase) - 1) for _ in range(len(msg)))
 
-  return ''.join(shift_char(char, shift) for char, shift in izip(msg, pad))
+    return ''.join(shift_char(char, shift) for char, shift in izip(msg, pad))
 
 def caesar_cipher(text, shift):
-  return ''.join(shift_char(c, shift) for c in text)
+    return ''.join(shift_char(c, shift) for c in text)
