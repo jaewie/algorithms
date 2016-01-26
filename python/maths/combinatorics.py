c66077ba39a5bb6d2fc4d28808b30e97fc58a2cc
Jae Wie <jaehyun.wie@mail.utoronto.ca>
2016-01-26 07:57:20 -0500

    add some proofs for combinatoric algorithms

--- python/maths/combinatorics.py
+++ python/maths/combinatorics.py
@@ -5,7 +5,29 @@ def shuffle(lst):
     '''Return a new list with items in lst rearranged randomly.'''
 
     # Fisher-Yate's algorithm
-    return [lst.pop(randint(0, len(lst) - 1)) for _ in range(len(lst))]
+    #
+    # Proof of correctness:
+    #
+    # Let N be # of elements in lst
+    # Loop invariant after ith iteration:
+    #
+    # - Each element has 1 / N chance of being placed on each of lst[0...i]
+    # - Each element has (N - 1 - i) / N chance of not being placed on any of lst[0...i]
+    #
+    # Base case: i = 0
+    #
+    # - j is between 0...N - 1
+    # - So chance of en element being placed on lst[0] is 1 / N, not being placed (N - 1) / N
+    #
+    # Generic case: i > 0 and assume loop variant holds before the ith iteration begins
+    #
+    # - j is between i...N - 1
+    # - Chance of an element not being chosen on lst[0...i - 1] is (N - 1 - (i - 1)) / N = (N - i) / N
+    # - So chance of an elemnt being placed on lst[i] is (N - i) / N * (1 / (N - i)) = 1 / N
+    # - So chance of not being placed there is (N - i) / N * (N - i - 1) / (N - i) = (N - 1 - i) / N
+    for i in range(len(lst)):
+        j = randint(i, len(lst) - 1)
+        lst[i], lst[j] = lst[j], lst[i]
 
 
 def sample(lst, k):
@@ -26,6 +48,17 @@ def sample(lst, k):
 def fair_coin(biased_coin):
     '''Return 0, 1 with 50% chance each'''
 
-    # Algorithm by John Von Neumann
+    # Algorithm by John von Neumann
     first, second = biased_coin(), biased_coin()
+
+    # +-----------------------------------------+
+    # |       | heads       | tails             |
+    # +-----------------------------------------+
+    # | heads | p * p       | p * (1 - p)       |
+    # | tails | p * (1 - p) | (1 - p) * (1 - p) |
+    # +-----------------------------------------+
+    #
+    # chance of first, second == (heads, tails) is the same as
+    # chance of first, second == (tails, heads): p * (1 - p)
+
     return first if first == second else fair_coin(biased_coin)
