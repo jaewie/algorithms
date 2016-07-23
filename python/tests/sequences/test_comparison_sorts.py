dc07af7ddea33103a0750576677d6d195cd3058e
Jae Wie <jaehyun.wie@mail.utoronto.ca>
2016-07-22 23:12:10 -0400

    refactor comparison sort test

--- python/tests/sequences/test_comparison_sorts.py
+++ python/tests/sequences/test_comparison_sorts.py
@@ -7,17 +7,18 @@ class TestComparisonSorts(unittest.TestCase):
     def test_select_sort_on_empty_list(self):
         self.assertEquals(selection_sort([]), [])
 
-
     def test_select_sort_on_one_element_list(self):
         self.assertEquals(selection_sort([1]), [1])
 
-
     def test_select_sort_on_multiple_element_list(self):
-        lst = [1, 5, 3]
-        self.assertEquals(selection_sort(lst), sorted(lst))
-
+        self._test_sort(selection_sort, [1, 5, 3])
 
     def test_select_sort_on_sorted_list(self):
-        lst = range(100)
-        self.assertEquals(selection_sort(lst), range(100))
+        self._test_sort(selection_sort, range(100))
+
+    def test_select_sort_on_reverse_sorted_list(self):
+        self._test_sort(selection_sort, list(reversed(range(100))))
+
+    def _test_sort(self, sort_func, example_list):
+        self.assertEquals(sort_func(example_list), sorted(example_list))
 
