012a3bf706cb81b0ca09420ed6e5a54aaffc7138
Jae Wie <jaehyun.wie@mail.utoronto.ca>
2016-07-28 05:43:58 -0400

    refactor dynamic array

--- python/collection/dynamic_array.py
+++ python/collection/dynamic_array.py
@@ -20,17 +20,13 @@ class DynamicArray(object):
 
     def append(self, ele):
         if self._size == self._capacity:
-            self._capacity *= 2
-            self._resize(self._capacity)
+            self._resize(self._capacity * 2)
 
         self._array[self._size] = ele
         self._size += 1
 
     def find(self, ele):
-        for i in range(self._size):
-            if self[i] == ele:
-                return i
-        return -1
+        return next((i for i in range(self._size) if self[i] == ele), -1)
 
     def pop(self):
         if not self._size:
@@ -40,14 +36,15 @@ class DynamicArray(object):
         item = self._array[self._size]
 
         if self._size > self._min_capacity and self._size / float(self._capacity) < 0.25:
-            self._capacity /= 2
-            self._resize(self._capacity)
+            self._resize(self._capacity / 2)
         return item
 
     def is_empty(self):
         return not len(self)
 
+
     def _resize(self, new_capacity):
+        self._capacity = new_capacity
         new_ary = zeros(new_capacity)
         for i in range(self._size):
             new_ary[i] = self._array[i]
